9e8f7e303de75b26747fe98b23756a23097a8e5a
Ptitet <ptitet73@gmail.com>
2026-07-19 14:50:06 +0200

    switched to hardware pwm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use rppal::gpio::Gpio;
+use rppal::{
+    gpio::Gpio,
+    pwm::{Channel, Polarity, Pwm},
+};
 use serde::Deserialize;
 use std::{fs, path::PathBuf, thread, time::Duration};
 
@@ -46,26 +49,52 @@ struct Daemon {
     config: PathBuf,
 }
 
+struct FanController(Pwm);
+impl FanController {
+    fn get_pwm_channel(bcm_pin: u8) -> Option<Channel> {
+        match bcm_pin {
+            12 | 18 => Some(Channel::Pwm0),
+            13 | 19 => Some(Channel::Pwm1),
+            _ => None,
+        }
+    }
+
+    pub fn new(bcm_pin: u8, frequency: f64) -> Result<Self, ()> {
+        if let Some(channel) = FanController::get_pwm_channel(bcm_pin) {
+            let pwm = Pwm::with_frequency(channel, frequency, 0., Polarity::Normal, false)
+                .expect("Could not initialize pwm");
+            Ok(FanController(pwm))
+        } else {
+            Err(()) // invalid pin
+        }
+    }
+
+    pub fn start(&self) {
+        self.0.enable().expect("Could not enable PWM channel");
+    }
+
+    pub fn set_speed(&self, speed: f64) {
+        self.0.set_duty_cycle(speed).expect("Could not set speed");
+    }
+}
+
 fn main() {
     let args = Daemon::parse();
     let config = Config::parse_from_file(&args.config);
 
-    let gpio = Gpio::new().expect("Could not create GPIO instance");
-    let mut pin = gpio
-        .get(config.bcm_pin)
-        .expect("Could not retrieve pin")
-        .into_output();
+    let controller = FanController::new(config.bcm_pin, config.pwm_frequency)
+        .expect("Invalid bcm pin for hardware pwm");
+    controller.start();
+
     match config.mode {
         Mode::Manual => {
-            pin.set_pwm_frequency(config.pwm_frequency, config.speed)
-                .expect("Could not set fan speed");
+            controller.set_speed(config.speed);
             println!("Set fan speed to {}", config.speed);
         }
         Mode::Auto => loop {
             let cpu_temp = get_cpu_temp();
             let duty_cycle = config.compute_speed_from_temp(cpu_temp);
-            pin.set_pwm_frequency(config.pwm_frequency, duty_cycle)
-                .expect("Could not set fan speed");
+            controller.set_speed(duty_cycle);
             println!("CPU temp : {cpu_temp}°C, fan speed : {duty_cycle}");
             thread::sleep(Duration::from_secs(config.sleep));
         },
